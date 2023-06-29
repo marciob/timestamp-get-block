e9add75c3fbb5e8ff378a5711aaf01ebf35b4cc8
m <13090422+marciob@users.noreply.github.com>
2023-06-28 23:43:29 -0300

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,94 @@
-import Image from 'next/image'
+"use client";
+
+import React, { useState } from "react";
+import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "tailwindcss/tailwind.css";
 
 export default function Home() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [block, setBlock] = useState(null);
+  const [isEstimate, setIsEstimate] = useState(false);
+
+  const getBlockNumber = async () => {
+    const timestamp = Math.floor(new Date(selectedDate).getTime() / 1000);
+    const url = `https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=before&apikey=YourApiKeyToken`;
+
+    if (timestamp > Math.floor(Date.now() / 1000)) {
+      const estimatedBlockNumber = await estimateFutureBlockNumber();
+      setIsEstimate(true);
+      setBlock(estimatedBlockNumber);
+      return;
+    }
+
+    setIsEstimate(false);
+
+    try {
+      const res = await axios.get(url);
+      if (res.data.status === "1") {
+        setBlock(res.data.result);
+      } else {
+        throw new Error(res.data.message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const estimateFutureBlockNumber = async () => {
+    const currentBlockNumberUrl =
+      "https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=YourApiKeyToken";
+    const currentBlockNumberResponse = await axios.get(currentBlockNumberUrl);
+    const currentBlockNumber = parseInt(
+      currentBlockNumberResponse.data.result,
+      16
+    ); // the response is in hex, so we convert it to decimal
+
+    const currentTimestamp = Math.floor(Date.now() / 1000); // current timestamp in seconds
+    const futureTimestamp = Math.floor(selectedDate.getTime() / 1000); // selected future timestamp in seconds
+
+    const secondsDifference = futureTimestamp - currentTimestamp;
+
+    const estimatedBlocks = Math.round(secondsDifference / 15); // Ethereum targets a block every ~15 seconds
+
+    const estimatedFutureBlockNumber = currentBlockNumber + estimatedBlocks;
+
+    return estimatedFutureBlockNumber;
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className="flex flex-col items-center pt-4 bg-gray-100 min-h-screen p-4">
+      <h1 className="text-3xl mb-6">Ethereum Block Finder</h1>
+      <p className="text-center mb-6 text-lg">
+        Select a date and time to find the corresponding Ethereum block number.
+      </p>
+      <div className="flex items-center space-x-4 mb-6">
+        <DatePicker
+          selected={selectedDate}
+          onChange={(date) => setSelectedDate(date)}
+          showTimeSelect
+          dateFormat="Pp"
+          className="p-2 border rounded-md border-gray-300"
         />
+        <button
+          className="px-6 py-2 bg-gray-300 text-gray-700 rounded hover:bg-gray-400 transition duration-200 ease-in-out"
+          onClick={getBlockNumber}
+        >
+          Get Block
+        </button>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      {block && (
+        <div className="mt-4">
+          <h2 className="text-lg mb-2">Block Number:</h2>
+          <p className="text-blue-500 text-2xl">{block}</p>
+          {isEstimate && (
+            <p className="text-sm text-gray-500">
+              This is an estimated block number for a future timestamp.
+            </p>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
